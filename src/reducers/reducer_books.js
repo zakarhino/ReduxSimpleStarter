89dd1fafe8fea182524b7a3dc813cdfa34cb837e
Zak Golding <hello@zakarhino.com>
2016-03-01 23:52:35 -0800

    Finished book

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'JavaScript: The Good Parts', pages: 109},
+    {title: 'Harry Potter', pages: 304},
+    {title: 'The Dark Tower', pages: 210},
+    {title: 'Eloquent Ruby', pages: 824},
+    {title: 'The Hobbit', pages: 403}
+  ]
+}
